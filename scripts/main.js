a450a004c704aa1155cf7f5fee5178a606693885
acorrington <acorrington1@byupathway.edu>
2025-07-08 10:15:43 -0700

    Add initial website structure and functionality
    - Created index.html with a basic layout including header, navigation, main content, and footer.
    - Added CSS styles for small and larger screens to ensure responsive design.
    - Implemented JavaScript for dynamic course filtering and navigation toggle.
    - Included SVG icons for social media links.
    - Established a course data structure in courses.js for managing course information.

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const navbutton = document.querySelector("#ham-btn");
+const mainNav = document.querySelector('#main-nav');
+
+navbutton.addEventListener("click", () => { 
+    navbutton.classList.toggle("show");  
+    mainNav.classList.toggle('show');
+});
+
+
